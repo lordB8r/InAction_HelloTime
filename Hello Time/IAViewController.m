75bb13f49c0ee3f12f345f5c0123030ae51d7828
lordB8r <jp@zavteq.com>
2013-10-08 01:21:29 -0500

    next steps

--- Hello Time/IAViewController.m
+++ Hello Time/IAViewController.m
@@ -9,6 +9,7 @@
 #import "IAViewController.h"
 
 @interface IAViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
 
 @end
 
@@ -18,12 +19,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self checkTime:self];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)checkTime:(id)sender
+{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"h:mm:ss a"];
+    [self.timeLabel setText:[formatter stringFromDate:[NSDate date]]];
+    
+    [self performSelector:@selector(checkTime:) withObject:self afterDelay:1.0];
+    
 }
 
 @end
